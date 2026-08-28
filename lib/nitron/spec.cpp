9fe5862b46442bed4ebabb8d4616a8ef863df089
BaraWazwaz <wazwazbaraa@gmail.com>
2026-08-28 14:19:23 +0300

    add: Spec and Test both respect the `this` keyword

--- lib/nitron/spec.cpp
+++ lib/nitron/spec.cpp
@@ -5,11 +5,9 @@ namespace nitron
 
 bool Test::displayResult(std::ostream& os, std::size_t tabs) const
 {
-    if (verdict)
-        os << std::string(tabs, '\t') << "[Passed]: { " << description << " }\n";
-    else
-        os << std::string(tabs, '\t') << "[Failed]: { " << description << " }\n";
-
+    os << std::string(tabs, '\t');
+    os << "[" << (this->verdict ? "Passed" : "Failed") << "]: ";
+    os << "{ " << this->description << " }\n";
     return verdict;
 }
 
@@ -23,17 +21,17 @@ Spec::Spec(std::string&& title) :
 {}
 
 Spec& Spec::openSubSpec(std::string&& title) {
-    children.emplace_back(std::forward<std::string>(title));
-    children.back().parent = this;
-    return children.back();
+    this->children.emplace_back(std::forward<std::string>(title));
+    this->children.back().parent = this;
+    return this->children.back();
 }
 
 Spec& Spec::closeSubSpec() {
-    return *parent;
+    return *this->parent;
 }
 
 Spec& Spec::addTest(Test&& test) {
-    direct.emplace_back(std::forward<Test>(test));
+    this->direct.emplace_back(std::forward<Test>(test));
     return *this;
 }
 
@@ -44,23 +42,22 @@ Spec& Spec::addSubSpec(Spec&& subSpec) {
 }
 
 bool Spec::displayResult(std::ostream& os) const {
-    return this->displayResult(os, 0);
+    return this->displayResult(os, 0ull);
 }
 
 bool Spec::displayResult(std::ostream& os, std::size_t tabs) const {
     bool verdict = true;
-    os << std::string(tabs, '\t') << "Spec { " << title << " } :\n";
-    
-    for (Test const& test : direct)
+    os << std::string(tabs, '\t');
+    os << "Spec { " << title << " } :\n";
+
+    for (const Test& test : this->direct)
         verdict = test.displayResult(os, tabs + 1) && verdict;
     
-    for (Spec const& spec : children)
+    for (const Spec& spec : this->children)
         verdict = spec.displayResult(os, tabs + 1) && verdict;
 
-    if (verdict)
-        os << std::string(tabs, '\t') << "Verdict: [Passed]\n";
-    else
-        os << std::string(tabs, '\t') << "Verdict: [Failed]\n";
+    os << std::string(tabs, '\t');
+    os << "Verdict: [" << (verdict ? "Passed" : "Failed") << "]\n";
 
     return verdict;
 }
@@ -71,7 +68,7 @@ Test Test::expectedToPass() & {
 }
 
 Test Test::expectedToPass() && {
-    return Test(verdict, std::move(description));
+    return Test(this->verdict, std::move(this->description));
 }
 
 Test Test::expectedToFail() & {
@@ -81,7 +78,7 @@ Test Test::expectedToFail() & {
 }
 
 Test Test::expectedToFail() && {
-    return Test(!verdict, std::move(description));
+    return Test(!this->verdict, std::move(this->description));
 }
 
 } // namespace nitron
